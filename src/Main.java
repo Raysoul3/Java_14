f8156ad5d9dcba1e19e46e5bddafb35a9c59767c
Raysoul3 <wuhuilei3@gmail.com>
2023-07-20 20:28:26 +0900

    第十四章クラスの内容の一部

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		Hero h = new Hero();
+		h.name = "Raysoul3";
+		Hero h2 = new Hero();
+		h2.name = "Raysoul3";
+		
+		
+		
+		System.out.println(h.equals(h2));
+		
+
+	}
+
+}
